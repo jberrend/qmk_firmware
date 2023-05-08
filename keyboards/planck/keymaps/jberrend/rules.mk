9b4da1da5682969240b43e721bc8d5398e50d6f4
Jonathan Berrend <jonberrend@gmail.com>
2023-05-07 22:55:45 -0700

    Change escape and boot keys.

--- keyboards/planck/keymaps/jberrend/rules.mk
+++ keyboards/planck/keymaps/jberrend/rules.mk
@@ -1,1 +1,23 @@
 SRC += muse.c
+
+### https://github.com/Felerius/planck-layout/blob/main/keymap/rules.mk
+# Disabled qmk features
+BOOTMAGIC_ENABLE = no
+MOUSEKEY_ENABLE = no
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+COMBO_ENABLE = no
+AUDIO_ENABLE = no
+RGBLIGHT_ENABLE = no
+LEADER_ENABLE = no
+MIDI_ENABLE = no
+UNICODE_ENABLE = no
+BLUETOOTH_ENABLE = no
+BACKLIGHT_ENABLE = no
+
+# Enabled qmk features
+EXTRAKEY_ENABLE = yes
+NKRO_ENABLE = yes
+
+# Decrease binary size using LTO
+LTO_ENABLE = no
